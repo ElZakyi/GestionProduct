524d5678c14a87ef7560f8779837f9b538db8b72
ElZakyi <zakariaelfoual0@gmail.com>
2025-11-19 07:25:09 +0100

    first commit

---
+++ gestProduit/src/test/java/com/sddi/gestProduit/GestProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sddi.gestProduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestProduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
